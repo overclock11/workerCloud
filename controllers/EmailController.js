f00f4543eb164606733ed2da3ec19fcf06dfeb65
Julian <jlian92@ghotmail.com>
2017-11-06 12:05:35 -0500

    sea agrega mailer para heroku

--- controllers/EmailController.js
+++ controllers/EmailController.js
@@ -3,10 +3,26 @@ var nodemailer = require('nodemailer');
 var config = require('../config');
 
 
+let api_key = process.env.MAILGUN_API_KEY;
+let domain = process.env.MAILGUN_DOMAIN;
+let mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+
 // email sender function
 exports.sendEmail = function(emailUser, url_master){
 
-    console.log("va a enviar correo a " + emailUser);
+    var data = {
+        from: config.configMailFrom.from,
+        to: emailUser,
+        subject:config.configMailFrom.subject,
+        text: config.configMailFrom.text
+    };
+
+    mailgun.messages().send(data, function (error, body) {
+        console.log(body);
+    });
+
+/*    console.log("va a enviar correo a " + emailUser);
     // Definimos el transporter
         var transporter = nodemailer.createTransport(config.awsSES);
     // Definimos el email
@@ -24,5 +40,5 @@ exports.sendEmail = function(emailUser, url_master){
         } else {
             console.log("Email sent");
         }
-    });
+    });*/
 };
